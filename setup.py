20279dd00ba071fbce224ec2bbc658c5acbe2d59
Donald Hall <142858254+Weifeng2829@users.noreply.github.com>
2026-07-11 22:57:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="wav_minigrid",
     version="0.1",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(where="src") + ["env"],
+    package_dir={
+        "": "src",
+        "env": "env",
+    },
+    install_requires=[
+        "gym>=0.9.6",
+        "numpy>=1.15.0",
+        "matplotlib",
+        "tqdm",
+    ],
 )
